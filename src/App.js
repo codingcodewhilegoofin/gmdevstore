a60d9dfcf80450e59b02775e56568d7a50f95067
Giovanni Moscato <69764323+codingcodewhilegoofin@users.noreply.github.com>
2022-04-05 12:00:30 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   //<Button variant="outlined" color="error">Error</Button>
   return (
     <div>
-      { ( siteVersion == "vanilla") ? <div style={{ padding:'10px'}}> <Button injectFirst size='small' fullWidth={true}  color="error" onClick={() =>  setSiteVersion("depedency")}> Turn off Dependency+ mode </Button> </div> : <ChoiceSite func={pull_data}/>  }
+      { ( siteVersion == "vanilla") ? <div style={{ padding:'0px', backgroundColor:'#242A36'}}> <Button variant="outlined" size='small' fullWidth={true}  color="error" onClick={() =>  setSiteVersion("depedency")}> Turn off Dependency+ mode </Button> </div> : <ChoiceSite func={pull_data}/>  }
       { ( siteVersion == "vanilla") ? <NewSite/> : <OldSite/> }
     </div>
     
--- src/newSite.js
+++ src/newSite.js
@@ -11,24 +11,35 @@ import { MapControls } from '@react-three/drei'
 
 function NewSite() {
     return(
-        /*The Canvas component does some 
+        /*
+        
+        The Canvas component does some 
         important setup work behind 
         the scenes: 
         
-        1:It sets up a Scene and a Camera, 
+        1:It sets up a Renderer,  Scene and a Camera, 
         the basic building blocks necessary for rendering
 
         2:It renders our scene every frame, 
         you do not need a traditional render-loop
 
+        Camera will create a Viewing Frustum: 
+        Everything inside the frustum is visible, while everything outside it is not. 
+
         */
         <div id="canvas-container" >
-            <Canvas > // 1: set up a Scene and a Camera
+            <Canvas
+                style={{background:'#1C2222'}}
+            > // 1: set up a Scene and a Camera
                 <MapControls/>
-                <ambientLight intensity={2} />
-                <directionalLight color="red" position={[0, 0, 5]} />
+                <ambientLight intensity={5} />
+                <directionalLight color="blue" position={[0, 0, 0]} />
                 <Box position={[1, 0, -10]} />
                 <Box position={[1, -50, -30]} />
+                <mesh>
+                    <boxBufferGeometry />
+                    <meshBasicMaterial color="#2F4845" transparent/>
+                </mesh>
             </Canvas>
         </div>
     );

e60f41abb22eada0f3a81f025a7fefbb00ca1eb4
Giovanni Moscato <69764323+codingcodewhilegoofin@users.noreply.github.com>
2022-07-15 12:19:46 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //Standard imports 
-import React, { Component, useState } from 'react';
+import React, { Component, useState , useEffect } from 'react';
 
 //MUI imports
 import Button from '@mui/material/Button';
@@ -15,25 +15,36 @@ import Start from './Start';
 import Spootify from '../src/icons/spootify.png';
 import BoxIcon from '../src/icons/3Dicon.svg';
 import SplineIcon from '../src/icons/Splinelogo.png';
-import Coinlib  from '../src/icons/CStracklogo/Coiblib.PNG';
+import Coinlib from '../src/icons/CStracklogo/Coiblib.PNG';
 
 function App() {
 
+  console.log(window.location.href);
+  console.log(typeof(window.location.href));
+
   const [siteVersion, setSiteVersion] = useState("");
   const [startScene, setStartScene] = useState(true);
+  
+
+  useEffect(()=> {
+    if (window.location.href == 'https://giomoscato.com/#/home') {
+      setStartScene(false);
+    }
+  },[])
+  
 
   const [modal, setModal] = useState(true);
 
-    const modalHandle = () => {
-        var x = document.getElementById("modalsection");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-            setModal(!modal);
-        }
-        else {
-            x.style.display = "none";
-        }
+  const modalHandle = () => {
+    var x = document.getElementById("modalsection");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      setModal(!modal);
     }
+    else {
+      x.style.display = "none";
+    }
+  }
 
   //Add drawer to hide return button 
 
@@ -43,6 +54,10 @@ function App() {
 
   const sceneChange = (data) => {
     setStartScene(data);
+  }
+
+  const homeRefresh = () => {
+    window.location.reload(false);
   }
 
   let theme = false;
@@ -58,19 +73,22 @@ function App() {
       <div id="modalsection" class="modal">
         <div class="modal-content">
           <button onClick={modalHandle} class="modalbtninner">❌</button>
-          <h1>Menu</h1>
+          <h1 className="modalTitle">Menu</h1>
 
-          <ul>
-            <li className='listmodal'> <a href='/#/' className='listmodal'> Home 🏠</a> </li>
-            <li className='listmodal'> <a href='/#/dependency+' className='listmodal'> 3JS / R3F <img style={{ backgroundColor: '#FFFFFF', padding: '2px' }} width='25px' height='25px'  src={BoxIcon}/></a> </li>
-            <li className='listmodal'> <a href='/#/splinescene' className='listmodal'> 3DSplineScene <img style={{ backgroundColor: '#FFFFFF', padding: '1px', marginLeft: '10px',marginTop:'5px' }} width='100px' height='100px'  src={SplineIcon}/></a> </li>
-            <li className='listmodal'> <a href='/#/projects'className='listmodal'> Projects </a> </li>
-            <li className='listmodal'> <a href='/#/Spotify' className='listmodal'> Spotify <img width='25px' height='25px'  src={Spootify}/></a> </li>
-            <li className='listmodal'> <a href='/#/cstrack' className='listmodal'> Stock&Crypto <img style={{ backgroundColor: '#FFFFFF', padding: '1px', marginLeft: '10px',marginTop:'5px' }} width='100px' height='100px'  src={Coinlib}/></a> </li>
-            <li className='listmodal'> <a href='/#/Dsa' className='listmodal' > DSA </a> </li>
-            <li className='listmodal'> <a href='/#/about' className='listmodal' > About </a> </li>
-            <li className='listmodal'> <a href='/#/contact' className='listmodal' > Contact </a> </li>
-          </ul>
+          <div className="modalBox">
+            <ul>
+              <li className='listmodal'> <a onClick={homeRefresh} href='/#/home' className='listmodal'> Home 🏠</a> </li>
+              <li className='listmodal'> <a href='/#/dependency+' className='listmodal'> 3JS / R3F <img style={{ backgroundColor: '#FFFFFF', padding: '2px' }} width='25px' height='25px' src={BoxIcon} /></a> </li>
+              <li className='listmodal'> <a href='/#/splinescene' className='listmodal'> 3DSplineScene <img style={{ backgroundColor: '#FFFFFF', padding: '1px', marginLeft: '10px', marginTop: '5px' }} width='100px' height='100px' src={SplineIcon} /></a> </li>
+              <li className='listmodal'> <a href='/#/projects' className='listmodal'> Projects </a> </li>
+              <li className='listmodal'> <a href='/#/Spotify' className='listmodal'> Spotify <img width='25px' height='25px' src={Spootify} /></a> </li>
+              <li className='listmodal'> <a href='/#/cstrack' className='listmodal'> Stock&Crypto <img style={{ backgroundColor: '#FFFFFF', padding: '1px', marginLeft: '10px', marginTop: '5px' }} width='100px' height='100px' src={Coinlib} /></a> </li>
+              <li className='listmodal'> <a href='/#/Dsa' className='listmodal' > DSA </a> </li>
+              <li className='listmodal'> <a href='/#/about' className='listmodal' > About </a> </li>
+              <li className='listmodal'> <a href='/#/contact' className='listmodal' > Contact </a> </li>
+            </ul>
+          </div>
+
         </div>
       </div>
       {(!startScene === true) ? <></> : <Start func={sceneChange} />}

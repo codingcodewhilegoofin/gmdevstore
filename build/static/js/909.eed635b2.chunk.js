8142a0d71d9ab33871c63ab34aceba6b3114f9df
codedumpsterfire <giomoscatodev@gmail.com>
2023-12-28 17:56:05 -0600

    updated

--- build/static/js/909.eed635b2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgmdevstore=self.webpackChunkgmdevstore||[]).push([[909],{2909:function(e,t,o){o.r(t);var a=o(184);t.default=function(e){return(0,a.jsx)("group",{name:"20170707201828",position:[486.29,-43.26,3],rotation:[-Math.PI/2,0,0],scale:20,children:(0,a.jsx)("mesh",{name:"1",geometry:e.nodes[1].geometry,material:e.nodes[1].material,castShadow:!0,receiveShadow:!0,position:[0,0,3]})})}}}]);
-//# sourceMappingURL=909.eed635b2.chunk.js.map
